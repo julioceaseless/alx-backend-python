236faf7aa9c03a6ebe885aae702f6d191c512866
julioceaseless <julioceaseless@gmail.com>
2024-07-10 07:19:23 +0000

    fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,12 +4,13 @@ Async generator
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Generate 0 - 10
     """
     for num in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
