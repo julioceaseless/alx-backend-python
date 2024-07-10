5402849d2e75d77fae388cf36a20cbc2e579cafc
julioceaseless <julioceaseless@gmail.com>
2024-07-10 07:06:03 +0000

    Async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Async generator
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Generate 0 - 10
+    """
+    for num in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,6 @@
+## 0x02 Pythob -Async Comprehension
+
+By completing these tasks, we demostrate the understanding on:
+- How to write an asynchronous generator
+- How to use async comprehensions
+- How to type-annotate generators
