3626ea8f659aca631b54bfc3d72b8927431d305a
julioceaseless <julioceaseless@gmail.com>
2024-08-03 05:27:42 +0300

    memoization and parameterization

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,9 +3,16 @@
 Unit tests
 """
 import unittest
+from typing import Dict, Tuple, Union
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-from utils import get_json, access_nested_map, memoize
+
+from utils import (
+    get_json,
+    access_nested_map,
+    memoize
+)
+
 
 # access_nested_map = __import__('utils').access_nested_map
 # get_json = __import__('utils').get_json
@@ -20,20 +27,71 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int]
+            ) -> None:
         """test access nested map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-       ({}, ("a",)),
-       ({"a": 1}, ("a", "b"))
+       ({}, ("a",), KeyError),
+       ({"a": 1}, ("a", "b"), KeyError)
        ])
-    def test_access_nested_map_exception(self, nested_map, path):
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception
+            ) -> None:
         """Check if KeyError is raised"""
-        with self.assertRaises(KeyError):
+        with self.assertRaises(exception):
             access_nested_map(nested_map, path)
 
 
+class TestGetJson(unittest.TestCase):
+    """Tests the `get_json` function."""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            ) -> None:
+        """Tests `get_json`'s output."""
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
+
+
+'''
 class TestGetJson(unittest.TestCase):
     """
     Mock http calls
@@ -43,7 +101,11 @@ class TestGetJson(unittest.TestCase):
         ("http://holberton.io", {"payload": False})
         ])
     @patch('utils.requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            mock_get):
         """ Mock test"""
         # define mock response data
         mock_response = Mock()
@@ -77,18 +139,18 @@ class TestMemoize(unittest.TestCase):
                           return_value=42) as mock_method:
             # create an instance of TestClass
             obj = TestClass()
-            
+
             # Call a_property twice
             result1 = obj.a_property
             result2 = obj.a_property
-            
+
             # Check that a_property returns the correct result
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
-            
+
             # Check that a_method is called only once
             mock_method.assert_called_once()
-
+'''
 
 if __name__ == "__main__":
     unittest.main()
